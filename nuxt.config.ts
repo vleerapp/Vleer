be4b3e698f08b1058477a1742c860c2b1069554c
PandaDΞV <nils.ulmann@klangkonzept.ch>
2023-04-09 12:19:17 +0200

    idk

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+import { defineNuxtConfig } from 'nuxt/config'
 
+export default defineNuxtConfig({
+  
 })
