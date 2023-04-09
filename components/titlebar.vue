8ffb2ef04a32287ecf78fbcbeedd66fd2550f84c
PandaDΞV <nils.ulmann@klangkonzept.ch>
2023-04-09 21:40:39 +0200

    test

--- components/titlebar.vue
+++ components/titlebar.vue
@@ -1,30 +1,10 @@
 <template>
   <div data-tauri-drag-region class="titlebar">
-    <div class="titlebar-button" id="titlebar-minimize">
-      <img src="https://api.iconify.design/mdi:window-minimize.svg" alt="minimize" />
-    </div>
-    <div class="titlebar-button" id="titlebar-maximize">
-      <img src="https://api.iconify.design/mdi:window-maximize.svg" alt="maximize" />
-    </div>
-    <div class="titlebar-button" id="titlebar-close">
-      <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
-    </div>
+    <img src="../assets/svg/linear/minimize.svg" class="titlebar-button" id="titlebar-minimize" />
+    <img src="../assets/svg/linear/maximize-2.svg" class="titlebar-button" id="titlebar-maximize" />
+    <img src="../assets/svg/linear/close.svg" class="titlebar-button" id="titlebar-close" />
   </div>
 </template>
-
-<script lang="js">
-import { appWindow } from '@tauri-apps/api/window'
-
-document
-  .getElementById('titlebar-minimize')
-  .addEventListener('click', () => appWindow.minimize())
-document
-  .getElementById('titlebar-maximize')
-  .addEventListener('click', () => appWindow.toggleMaximize())
-document
-  .getElementById('titlebar-close')
-  .addEventListener('click', () => appWindow.close())
-</script>
 
 <style>
 @import '~/css/titlebar.css';
