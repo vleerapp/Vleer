2e61f59a9af2dd699b67153fd5a68598dd5dd121
PandaDΞV <nils.ulmann@klangkonzept.ch>
2023-04-09 12:18:12 +0200

    added titlebar as component

---
+++ components/titlebar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div data-tauri-drag-region class="titlebar">
+    <div class="titlebar-drag"></div>
+    <div class="titlebar-button" id="titlebar-close"></div>
+    <div class="titlebar-button" id="titlebar-minimize"></div>
+    <div class="titlebar-button" id="titlebar-maximize"></div>
+  </div>
+</template>
+
+<style>
+@import '~/css/titlebar.css';
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -12,5 +12,4 @@ export default {
 
 <style>
 @import '~/css/style.css';
-@import '~/css/titlebar.css';
 </style>
