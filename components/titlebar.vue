7cfaec5c095970ba9e041d0444c5b01f44bb1244
PandaDΞV <nils.ulmann@klangkonzept.ch>
2023-04-09 13:43:39 +0200

    finally draggable window

--- components/titlebar.vue
+++ components/titlebar.vue
@@ -1,11 +1,30 @@
 <template>
   <div data-tauri-drag-region class="titlebar">
-    <div class="titlebar-drag"></div>
-    <div class="titlebar-button" id="titlebar-close"></div>
-    <div class="titlebar-button" id="titlebar-minimize"></div>
-    <div class="titlebar-button" id="titlebar-maximize"></div>
+    <div class="titlebar-button" id="titlebar-minimize">
+      <img src="https://api.iconify.design/mdi:window-minimize.svg" alt="minimize" />
+    </div>
+    <div class="titlebar-button" id="titlebar-maximize">
+      <img src="https://api.iconify.design/mdi:window-maximize.svg" alt="maximize" />
+    </div>
+    <div class="titlebar-button" id="titlebar-close">
+      <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
+    </div>
   </div>
 </template>
+
+<script lang="js">
+import { appWindow } from '@tauri-apps/api/window'
+
+document
+  .getElementById('titlebar-minimize')
+  .addEventListener('click', () => appWindow.minimize())
+document
+  .getElementById('titlebar-maximize')
+  .addEventListener('click', () => appWindow.toggleMaximize())
+document
+  .getElementById('titlebar-close')
+  .addEventListener('click', () => appWindow.close())
+</script>
 
 <style>
 @import '~/css/titlebar.css';
