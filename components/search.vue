fa9cf872acc16fd74da77afb126674b4ec4e8d87
PandaDΞV <nils.ulmann@klangkonzept.ch>
2023-04-10 10:36:40 +0200

    added search bar as component

---
+++ components/search.vue
@@ -0,0 +1,21 @@
+<template>
+  <input type="text" class="search" placeholder="Search" />
+</template>
+
+<style>
+.search {
+  width: 325px;
+  height: 40px;
+  background-color: #1e1d23;
+  border: 1px solid #26252b;
+  border-radius: 10px;
+  font-family: "Inter", sans-serif;
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+  padding-inline: 10px;
+  color: var(--tx);
+  outline: none;
+  margin-bottom: 28px;
+}
+</style>
