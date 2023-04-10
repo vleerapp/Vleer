12b67d2624ff5e86e59c641ddd12eb7846b636bd
PandaDΞV <nils.ulmann@klangkonzept.ch>
2023-04-10 10:36:12 +0200

    created page layout

---
+++ layouts/page.vue
@@ -0,0 +1,15 @@
+<template>
+  <sidebar />
+  <div class="content">
+    <search />
+    <slot />
+  </div>
+</template>
+
+<style>
+.content {
+  position: absolute;
+  top: 40px;
+  left: 372px;
+}
+</style>
