37ca52accbdca79f759a833644d6dcae7432ce10
PandaDΞV <nils.ulmann@klangkonzept.ch>
2023-04-10 17:16:49 +0200

    fixed titlebar icons

--- components/titlebar.vue
+++ components/titlebar.vue
@@ -1,32 +1,39 @@
 <template>
   <div data-tauri-drag-region class="titlebar">
-    <svg width="18" height="18" viewBox="0 0 24 24" fill="none" class="titlebar-button" id="titlebar-minimize"
-      xmlns="http://www.w3.org/2000/svg">
-      <path d="M2 9V6.5C2 4.01 4.01 2 6.5 2H9" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"
-        stroke-linejoin="round" />
-      <path d="M15 2H17.5C19.99 2 22 4.01 22 6.5V9" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"
-        stroke-linejoin="round" />
-      <path d="M22 16V17.5C22 19.99 19.99 22 17.5 22H16" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"
-        stroke-linejoin="round" />
-      <path d="M9 22H6.5C4.01 22 2 19.99 2 17.5V15" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"
-        stroke-linejoin="round" />
-    </svg>
+    <div class="titlebar-button" id="titlebar-minimize">
+      <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M6 12H18" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+      </svg>
+    </div>
 
-    <svg id="titlebar-minimize" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M2 9V6.5C2 4.01 4.01 2 6.5 2H9" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"
-        stroke-linejoin="round" />
-      <path d="M15 2H17.5C19.99 2 22 4.01 22 6.5V9" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"
-        stroke-linejoin="round" />
-      <path d="M22 16V17.5C22 19.99 19.99 22 17.5 22H16" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"
-        stroke-linejoin="round" />
-      <path d="M9 22H6.5C4.01 22 2 19.99 2 17.5V15" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"
-        stroke-linejoin="round" />
-    </svg>
+    <div class="titlebar-button" id="titlebar-maximize">
+      <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M2 9V6.5C2 4.01 4.01 2 6.5 2H9" stroke="#ffffff" stroke-width="2" stroke-linecap="round"
+          stroke-linejoin="round" />
+        <path d="M15 2H17.5C19.99 2 22 4.01 22 6.5V9" stroke="#ffffff" stroke-width="2" stroke-linecap="round"
+          stroke-linejoin="round" />
+        <path d="M22 16V17.5C22 19.99 19.99 22 17.5 22H16" stroke="#ffffff" stroke-width="2" stroke-linecap="round"
+          stroke-linejoin="round" />
+        <path d="M9 22H6.5C4.01 22 2 19.99 2 17.5V15" stroke="#ffffff" stroke-width="2" stroke-linecap="round"
+          stroke-linejoin="round" />
+      </svg>
+    </div>
 
-
-    <img src="../assets/svg/linear/minimize.svg" stroke="white" class="titlebar-button" id="titlebar-minimize" />
-    <img src="../assets/svg/linear/maximize.svg" class="titlebar-button" id="titlebar-maximize" />
-    <img src="../assets/svg/linear/close.svg" class="titlebar-button" id="titlebar-close" />
+    <div class="titlebar-button" id="titlebar-close">
+      <svg width="18" height="18" viewBox="0 0 24 24" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink"
+        xmlns="http://www.w3.org/2000/svg">
+        <g id="close" fill-opacity="1">
+          <path d="M0 0L24 0L24 24L0 24L0 0Z" id="close" fill="none" fill-rule="evenodd" stroke="none" />
+          <g id="close">
+            <path d="M2 22L22 2" id="Vector" fill="none" fill-rule="evenodd" stroke="#fff" stroke-width="2"
+              stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M22 22L2 2" id="Vector" fill="none" fill-rule="evenodd" stroke="#fff" stroke-width="2"
+              stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M24 0L24 24L0 24L0 0L24 0Z" id="Vector" fill="none" fill-rule="evenodd" stroke="none" />
+          </g>
+        </g>
+      </svg>
+    </div>
   </div>
 </template>
 
