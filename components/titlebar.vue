9055a63237f4a5eb0d1cbf1f44cf4ff5b53d8b7a
Waradu <waradu@outlook.com>
2023-04-13 16:32:34 +0200

    fixed scrollbar + fade out

--- components/titlebar.vue
+++ components/titlebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="titlebar">
+  <div data-tauri-drag-region class="titlebar">
     <div class="titlebar-button" id="titlebar-minimize">
       <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M6 12H18" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -34,8 +34,8 @@
         </g>
       </svg>
     </div>
-    <div class="dragRegion" data-tauri-drag-region></div>
   </div>
+  <div class="fadeOut"></div>
 </template>
 
 <style>
