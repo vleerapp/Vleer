88285311f5f8322f0aa0c52977c52a1e02a397ed
PandaDΞV <nils.ulmann@klangkonzept.ch>
2023-04-13 21:40:13 +0200

    formated

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="sidebar" id="sidebar">
     <div class="minibar">
-      <img src="~/assets/waradu.png" alt="userimg" class="user-img" />
+      <img src="~/assets/pandadev.png" alt="userimg" class="user-img" />
       <div class="settings">
         <img src="/svg/linear/setting-2.svg" width="18" height="18" />
       </div>
@@ -9,48 +9,31 @@
     <div id="sidenav_tabs">
       <div class="tab">
         <nuxt-link to="/" class="tab-link">
-          <div
-            style="--img: url('/svg/linear/home-2.svg')"
-            class="tab_icon home_icon"
-          ></div>
+          <div style="--img: url('/svg/linear/home-2.svg')" class="tab_icon home_icon"></div>
           <span class="tab_text">Home</span>
         </nuxt-link>
       </div>
       <div class="tab">
         <nuxt-link to="/songs" class="tab-link">
-          <div
-            style="--img: url('/svg/linear/music.svg')"
-            class="tab_icon songs_icon"
-          ></div>
+          <div style="--img: url('/svg/linear/music.svg')" class="tab_icon songs_icon"></div>
           <span class="tab_text">Songs</span>
         </nuxt-link>
       </div>
       <div class="tab">
         <nuxt-link to="/playlists" class="tab-link">
-          <div
-            style="--img: url('/svg/linear/note.svg')"
-            class="tab_icon playlists_icon"
-          ></div>
+          <div style="--img: url('/svg/linear/note.svg')" class="tab_icon playlists_icon"></div>
           <span class="tab_text">Playlists</span>
         </nuxt-link>
       </div>
       <div class="tab">
         <nuxt-link to="/library" class="tab-link">
-          <div
-            style="--img: url('/svg/linear/video-square.svg')"
-            class="tab_icon library_icon"
-          ></div>
+          <div style="--img: url('/svg/linear/video-square.svg')" class="tab_icon library_icon"></div>
           <span class="tab_text">Library</span>
         </nuxt-link>
       </div>
     </div>
-    <div
-      id="minimizer"
-      style="--arrow: url('/svg/linear/arrow-close.svg')"
-      v-on:click="toggleSidebar"
-      class="minimizeSidebar"
-      alt=""
-    ></div>
+    <div id="minimizer" style="--arrow: url('/svg/linear/arrow-close.svg')" v-on:click="toggleSidebar"
+      class="minimizeSidebar" alt=""></div>
   </div>
 </template>
 
