97011f8f77a62658cf6082521b6e39ae8e84934f
PandaDΞV <nils.ulmann@klangkonzept.ch>
2023-05-15 20:43:23 +0200

    added placeholder avatar

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="sidebar animation" id="sidebar" :class="minimized ? 'minimized' : ''">
     <div class="minibar">
+      <img id="avatar-img" src="empty.png" />
       <nuxt-link class="settings" to="/settings">
         <img src="/svg/linear/setting-2.svg" width="18" height="18" />
       </nuxt-link>
@@ -8,49 +9,31 @@
     <div id="sidenav_tabs">
       <div class="tab">
         <nuxt-link to="/" class="tab-link">
-          <div
-            style="--img: url('/svg/linear/home-2.svg')"
-            class="tab_icon home_icon"
-          ></div>
+          <div style="--img: url('/svg/linear/home-2.svg')" class="tab_icon home_icon"></div>
           <span class="tab_text">Home</span>
         </nuxt-link>
       </div>
       <div class="tab">
         <nuxt-link to="/songs" class="tab-link">
-          <div
-            style="--img: url('/svg/linear/music.svg')"
-            class="tab_icon songs_icon"
-          ></div>
+          <div style="--img: url('/svg/linear/music.svg')" class="tab_icon songs_icon"></div>
           <span class="tab_text">Songs</span>
         </nuxt-link>
       </div>
       <div class="tab">
         <nuxt-link to="/playlists" class="tab-link">
-          <div
-            style="--img: url('/svg/linear/note.svg')"
-            class="tab_icon playlists_icon"
-          ></div>
+          <div style="--img: url('/svg/linear/note.svg')" class="tab_icon playlists_icon"></div>
           <span class="tab_text">Playlists</span>
         </nuxt-link>
       </div>
       <div class="tab">
         <nuxt-link to="/library" class="tab-link">
-          <div
-            style="--img: url(/svg/linear/video-square.svg)"
-            class="tab_icon library_icon"
-          ></div>
+          <div style="--img: url(/svg/linear/video-square.svg)" class="tab_icon library_icon"></div>
           <span class="tab_text">Library</span>
         </nuxt-link>
       </div>
     </div>
-    <div
-      id="minimizer"
-      style="--arrow: url('/svg/linear/arrow-close.svg')"
-      v-on:click="toggleSidebar"
-      class="minimizeSidebar animation"
-      :class="minimized ? 'mini' : ''"
-      alt=""
-    ></div>
+    <div id="minimizer" style="--arrow: url('/svg/linear/arrow-close.svg')" v-on:click="toggleSidebar"
+      class="minimizeSidebar animation" :class="minimized ? 'mini' : ''" alt=""></div>
   </div>
 </template>
 
@@ -62,14 +45,39 @@ import {
   exists,
   readTextFile,
 } from "@tauri-apps/api/fs";
+import { convertFileSrc } from "@tauri-apps/api/tauri";
+import { audioDir, join } from "@tauri-apps/api/path";
+
+interface Contents {
+  avatarPath: String;
+}
 
 var contents = await readTextFile("config.json", {
   dir: BaseDirectory.AppConfig,
 });
 
+var parsedContents = JSON.parse(contents) as Contents;
+
+async function setAvatar() {
+  /* console.log("asd")
+  const avatar = document.createElement("source");
+  const img = document.getElementById("avatar-img");
+  avatar.type = "image";
+  avatar.src = JSON.stringify(parsedContents["avatarPath"]);
+  if (img != null) {
+    img.innerHTML = "";
+    img.appendChild(avatar);
+  } */
+
+  const img = document.getElementById("avatar-img");
+  img?.style.borderImageSource("waradu.png")
+  console.log("asda")
+
+}
+
 contents = JSON.parse(contents);
 
-var minimized = contents.miniSidebar;
+var minimized = contents["miniSidebar"];
 
 async function toggleSidebar() {
   var element = document.getElementById("sidebar");
@@ -108,6 +116,8 @@ async function saveFile(contents: JSON) {
     console.log(err);
   }
 }
+
+setAvatar()
 </script>
 
 <style>
