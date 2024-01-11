50a8dcb7f3ace15a8f817d88c4cd36accd194ef5
pandadev <70103896+0PandaDEV@users.noreply.github.com>
2024-01-11 16:07:57 +0100

    style

--- app.vue
+++ app.vue
@@ -8,5 +8,15 @@
 <style>
 body{
   background-color: black;
+  color: white !important;
+  font-family: Inter;
+  text-decoration: none !important;
+  font-size: 16px;
+}
+
+*{
+  font-family: Inter;
+  text-decoration: none !important;
+  font-size: 16px;
 }
 </style>
