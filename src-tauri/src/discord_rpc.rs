db6273276183ffd0cc0be28e7a131e230bdeac62
pandadev <70103896+0PandaDEV@users.noreply.github.com>
2024-01-11 21:48:18 +0100

    added discord rpc

---
+++ src-tauri/src/discord_rpc.rs
@@ -0,0 +1,32 @@
+// src/discord_rpc.rs
+
+use discord_rpc_client::Client;
+use discord_rpc_client::models::rich_presence::{Activity, ActivityAssets};
+use std::sync::Mutex;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref DRPC_CLIENT: Mutex<Client> = Mutex::new(Client::new(1194990403963858984));
+}
+
+pub fn initialize_rpc() {
+    let mut drpc = DRPC_CLIENT.lock().unwrap();
+    drpc.start();
+}
+
+#[tauri::command]
+pub fn update_activity(state: String, details: String, large_image: String, large_image_text: String, small_image: String, small_image_text: String) {
+    let mut drpc = DRPC_CLIENT.lock().unwrap();
+
+    let activity = Activity::new()
+        .state(state)
+        .details(details)
+        .assets(|_| ActivityAssets::new()
+            .large_image(large_image)
+            .large_text(large_image_text)
+            .small_image(small_image)
+            .small_text(small_image_text)
+        );
+
+    drpc.set_activity(|_| activity).expect("Failed to set activity");
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,34 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use discord_rpc_client::models::rich_presence::{Activity, ActivityAssets};
-use discord_rpc_client::Client;
-use std::{thread, time::Duration};
+mod discord_rpc;
 
 fn main() {
-    thread::spawn(|| {
-        let mut drpc = Client::new(1194990403963858984);
-        drpc.start();
+    // Initialize the Discord RPC client
+    discord_rpc::initialize_rpc();
 
-        loop {
-            let activity = Activity::new()
-                .state("Your state message")
-                .details("Your details message")
-                .assets(|_| {
-                    ActivityAssets::new()
-                        .large_image("logo")
-                        .large_text("Text shown when hovering over large image")
-                        .small_image("small_logo")
-                        .small_text("Text shown when hovering over small image")
-                });
-
-            drpc.set_activity(|_| activity)
-                .expect("Failed to set activity");
-            thread::sleep(Duration::from_secs(10));
-        }
-    });
-
+    // Start the Tauri application
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![discord_rpc::update_activity])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
