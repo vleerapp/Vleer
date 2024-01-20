ea1afcaa1ee396011e475f64e69ea2bcb21eec2b
pandadev <70103896+0PandaDEV@users.noreply.github.com>
2024-01-20 17:41:57 +0100

    fixed discord rpc lagging everything out

--- src-tauri/src/discord_rpc.rs
+++ src-tauri/src/discord_rpc.rs
@@ -4,6 +4,7 @@ use discord_rpc_client::Client;
 use discord_rpc_client::models::rich_presence::{Activity, ActivityAssets};
 use std::sync::Mutex;
 use lazy_static::lazy_static;
+use std::thread;
 
 lazy_static! {
     static ref DRPC_CLIENT: Mutex<Client> = Mutex::new(Client::new(1194990403963858984));
@@ -16,17 +17,19 @@ pub fn initialize_rpc() {
 
 #[tauri::command]
 pub fn update_activity(state: String, details: String, large_image: String, large_image_text: String, small_image: String, small_image_text: String) {
-    let mut drpc = DRPC_CLIENT.lock().unwrap();
+    thread::spawn(move || {
+        let mut drpc = DRPC_CLIENT.lock().unwrap();
 
-    let activity = Activity::new()
-        .state(state)
-        .details(details)
-        .assets(|_| ActivityAssets::new()
-            .large_image(large_image)
-            .large_text(large_image_text)
-            .small_image(small_image)
-            .small_text(small_image_text)
-        );
+        let activity = Activity::new()
+            .state(state)
+            .details(details)
+            .assets(|_| ActivityAssets::new()
+                .large_image(large_image)
+                .large_text(large_image_text)
+                .small_image(small_image)
+                .small_text(small_image_text)
+            );
 
-    drpc.set_activity(|_| activity).expect("Failed to set activity");
+        drpc.set_activity(|_| activity).expect("Failed to set activity");
+    });
 }
