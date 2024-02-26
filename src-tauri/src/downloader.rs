a684ad8aa7070c4b18713e7f39a177c85f0a1271
pandadev <70103896+0PandaDEV@users.noreply.github.com>
2024-02-26 18:03:41 +0100

    added check if folder extist

--- src-tauri/src/downloader.rs
+++ src-tauri/src/downloader.rs
@@ -10,16 +10,22 @@ pub async fn download(url: String, name: String) {
         "macos" | "linux" => {
             let username = std::env::var("USER").unwrap_or_else(|_| "default".into());
             path.push(format!("/users/{}/Music/Vleer", username));
+            if !path.exists() {
+                std::fs::create_dir_all(&path).unwrap();
+            }
         },
         "windows" => {
             let username = std::env::var("USERNAME").unwrap_or_else(|_| "default".into());
             path.push(format!("C:\\Users\\{}\\Music\\Vleer", username));
+            if !path.exists() {
+                std::fs::create_dir_all(&path).unwrap();
+            }
         },
         _ => {}
     }
     path.push(&name);
 
-    println!("{}", &path.display());
-
     video.download(&path).await.unwrap();
+    
+    println!("{}", &path.display());
 }
