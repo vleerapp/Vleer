3f78e14f80b3543bf357f6010189f6c0e40dad7b
pandadev <70103896+0PandaDEV@users.noreply.github.com>
2024-04-18 00:38:39 +0200

    annoying rust stuff

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,10 +13,10 @@ mod discord_rpc;
 #[tauri::command]
 async fn download(url: String) -> Result<(), TauriError> {
 
-    let ytId = url.trim_start_matches("https://youtube.com/watch?v=");
+    let yt_id = url.trim_start_matches("https://youtube.com/watch?v=");
 
     let client = reqwest::Client::new();
-    let response = client.get(format!("https://wave.wireway.ch/api/extract/music?q=/watch?v={}", ytId))
+    let response = client.get(format!("https://wave.wireway.ch/api/extract/music?q=/watch?v={}", yt_id))
         .send()
         .await
         .map_err(|e| anyhow!("Failed to send request: {}", e))?;
@@ -52,7 +52,7 @@ async fn download(url: String) -> Result<(), TauriError> {
 
     let mut path = base_path.clone();
     path.push("Songs");
-    path.push(format!("{}.webm", ytId));
+    path.push(format!("{}.webm", yt_id));
 
     let mut file = tokio::fs::File::create(&path).await.map_err(|e| anyhow!("Failed to create file: {}", e))?;
     let content = video_response.bytes().await.map_err(|e| anyhow!("Failed to read video bytes: {}", e))?;
