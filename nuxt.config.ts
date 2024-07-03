80f2e85755e0585c2740547af62e02fd7bd42a84
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2024-07-03 14:08:57 +0200

    overflow fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: ['@pinia/nuxt'],
-  ssr: false
+  ssr: false,
+  app: {
+    head: {
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+        }
+      ]
+    },
+  },
 })
