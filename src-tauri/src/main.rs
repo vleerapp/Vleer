243150d230256d32945b2c2da988044b2e2bf48a
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2024-09-17 17:35:02 +1000

    restrucutre

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,12 @@
     windows_subsystem = "windows"
 )]
 
-mod db;
-mod utils;
+mod api;
 
 use tauri_plugin_prevent_default::Flags;
 
 fn main() {
-    let _ = utils::discord_rpc::connect_rpc();
+    let _ = api::discord_rpc::connect_rpc();
 
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
@@ -21,35 +20,24 @@ fn main() {
                 .with_flags(Flags::all().difference(Flags::CONTEXT_MENU))
                 .build(),
         )
-        // .plugin(
-        //     tauri_plugin_sql::Builder::default()
-        //         .add_migrations("sqlite:data.db", db::migration::load_migrations())
-        //         .build(),
-        // )
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .setup(|app| {
             let app_handle = app.handle();
-
-            let config_path = utils::commands::get_config_path();
-            tauri::async_runtime::spawn(async move {
-                if let Err(e) = db::migration::insert_data(config_path).await {
-                    eprintln!("Error inserting data: {:?}", e);
-                }
-            });
+            let config_path = api::commands::get_config_path();
 
             let update_handle = app_handle.clone();
             tauri::async_runtime::spawn(async move {
-                utils::updater::check_for_updates(update_handle).await;
+                api::updater::check_for_updates(update_handle).await;
             });
 
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![
-            utils::discord_rpc::update_activity,
-            utils::discord_rpc::clear_activity,
-            utils::commands::download,
-            utils::commands::ping_urls,
-            utils::commands::get_music_path
+            api::discord_rpc::update_activity,
+            api::discord_rpc::clear_activity,
+            api::commands::download,
+            api::commands::ping_urls,
+            api::commands::get_music_path
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
